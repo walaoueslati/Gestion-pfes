c86f914a93cff7e4ffc392095fc68ee559488a5b
unknown <bhksalma0@gmail.com>
2025-04-15 16:46:43 +0200

    final

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -6,13 +6,14 @@ import Sidebar from './Sidebar';
 const Layout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(true);
 
+  
   return (
     <div>
       <Navbar onToggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
       <div className="flex">
         <div className="min-h-screen bg-gray-100"> 
           <Sidebar />
-          
+
         </div>
         <div className="w-3/4 p-4"> 
         {sidebarOpen && (
